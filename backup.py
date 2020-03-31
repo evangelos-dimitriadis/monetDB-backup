4dcaea9fbd431a41a3cd926bd162cdb67a6dcd81
Evangelos Dimitriadis <evangelos.dimitriadis@protonmail.com>
2020-03-31 17:54:41 +0200

    Added ValueError, change some static content to vars. Fix credentials check.

--- backup.py
+++ backup.py
@@ -7,7 +7,7 @@ import random
 import re
 
 backup_path = '/home/dameko/Documents/monetdb-backup/'
-dbfarm_path = '/home/dameko/Documents/replication_torture/dbfarm_master/'
+dbfarm_path = '/home/dameko/Documents/monetdb-s3/s3dbfarm'
 days_gap = 1
 
 if backup_path[-1] != '/':
@@ -39,10 +39,10 @@ def get_file_names(folder_path):
         raise FileNotFoundError
 
 
-def create_backup(backup_file):
+def create_backup(backup_file, dbname):
 
     with open(backup_file, "a") as outfile:
-        backup = Popen(["msqldump", "-N", "-d", "master"], stdout=PIPE)
+        backup = Popen(["msqldump", "-N", "-d", dbname], stdout=PIPE)
         Popen(["gzip"], stdin=backup.stdout, stdout=outfile)
         backup.stdout.close()
 
@@ -59,11 +59,14 @@ def find_latest_backup(backup_path, db_name):
         if re.search(r'(.*_)[A-Z 0-9]{6}(_backup.sql.gz)$', backup_file):
             try:
                 with gzip.open(backup_file, 'r') as file:
+                    # Read the timestamp from the first line
                     first_data = file.peek(10).decode("utf-8")
                     timestamp = datetime.strptime(first_data, '-- %d-%m-%Y %H:%M:%S\n')
                     backups_dict[backup_file] = timestamp
                     print(timestamp)
                     file.close()
+            except ValueError:
+                print(backup_file + " may be corrupted, no timestamp found.")
             except FileNotFoundError:
                 pass
 
@@ -74,26 +77,25 @@ def find_latest_backup(backup_path, db_name):
                key=lambda v: v if isinstance(v, datetime) else datetime.max)
 
 
-# Check if the credentials are correct
-cmd = check_output(["mclient", "-d", "master", "--statement", "SELECT 'True';", "--format", "csv"])
-
-if 'True' not in cmd.decode("utf-8"):
-    print("Couldn't connect to the database. Check your credentials.")
-    exit(1)
-
-
 db_names = get_db_names(dbfarm_path)
+
 for name in db_names:
+
+    # Check if the credentials are correct
+    cmd = check_output(["mclient", "-d", name, "--statement", "SELECT 'True';", "--format", "csv"])
+    if 'True' not in cmd.decode("utf-8"):
+        print("Couldn't connect to the database {0}. Check your credentials.".format(name))
+        continue
 
     # Check timestamps of backups, decide if a backup is needed
     latest_backup_tmsp = find_latest_backup(backup_path, name)
     print('Latest', name, '--', latest_backup_tmsp)
     if latest_backup_tmsp:
-        delta = datetime.now() - timedelta(hours=1 * days_gap)
+        delta = datetime.now() - timedelta(hours=24 * days_gap)
         if latest_backup_tmsp < delta:
             backup_file = backup_path + name + '_' + random_string() + '_backup.sql.gz'
-            create_backup(backup_file)
+            create_backup(backup_file, name)
 
     else:
         backup_file = backup_path + name + '_' + random_string() + '_backup.sql.gz'
-        create_backup(backup_file)
+        create_backup(backup_file, name)
